71018d69915cd9f9224648d17122db5060ea3752
Ov <ov@annexe3.com>
2014-02-03 14:13:19 +0100

    feat(interface) simpler

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var util = require('util'),
-    EventEmitter = require('events').EventEmitter;
+var EventEmitter = require('events').EventEmitter;
 
 function params(f) {
   var decl = f.toString();
@@ -10,30 +9,38 @@ function proj(x) {
   return this[x];
 }
 
-function Ctx() {
-  EventEmitter.call(this);
+var emitter = new EventEmitter();
+emitter.setMaxListeners(0);
 
-  this.pendings = 0;
-  this.errors = {};
+var pendings = 0;
+var errors = {};
+var values = {};
 
-  this.setMaxListeners(0);
-}
-util.inherits(Ctx, EventEmitter);
+var provide = function (name, err, value) {
+  if (err) errors[name] = err;
+  values[name] = value;
+  emitter.emit('$binding', name, err, value);
+  emitter.emit(name, name, err, value);
+  if (--pendings == 0) {
+    if (values.done) {
+      var ecount = Object.getOwnPropertyNames(errors).length;
+      values.done(ecount === 0 ? null : errors, values);
+    }
+  }
+};
 
-Ctx.prototype.define = function (name, factory) {
-  var self = this;
-
+var define = function (name, factory) {
   var dependencies = params(factory);
 
   var async = dependencies[dependencies.length - 1] == 'done';
   if (async) dependencies.pop();
 
-  self.pendings++;
+  pendings++;
 
-  var pendings = dependencies.length + 1;
+  var _pendings = dependencies.length + 1;
 
   var done = function (err, product) {
-    self.provide(name, err, product);
+    provide(name, err, product);
   };
 
   var ecount = 0;
@@ -41,49 +48,36 @@ Ctx.prototype.define = function (name, factory) {
   var solve = function (_, err, _) {
     if (err) ecount++;
 
-    if (--pendings == 0) {
+    if (--_pendings == 0) {
       if (ecount !== 0) {
         return done(new Error(ecount + " dependencies in error"));
       }
 
-      var args = dependencies.map(proj, self);
+      var args = dependencies.map(proj, values);
 
       if (async) {
         args.push(done);
-        return factory.apply(self, args);
+        return factory.apply(values, args);
       } else {
-        return done(null, factory.apply(self, args));
+        return done(null, factory.apply(values, args));
       }
     }
   };
 
   var listen = function (dependency) {
-    self.once(dependency, solve);
+    emitter.once(dependency, solve);
   };
 
   listen('@');
   dependencies.forEach(listen);
 };
 
-Ctx.prototype.provide = function (name, err, value) {
-  if (err) this.errors[name] = err;
-  this[name] = value;
-  this.emit('$binding', name, err, value);
-  this.emit(name, name, err, value);
-  if (--(this.pendings) == 0) {
-    if (this.done) {
-      var ecount = Object.getOwnPropertyNames(this.errors).length;
-      this.done(ecount === 0 ? null : this.errors, this);
-    }
-  }
+var resolve = function (done) {
+  emitter.emit('@');
 };
 
-Ctx.prototype.resolve = function (done) {
-  this.done = done;
-  this.emit('@');
-};
-
-module.exports = function () {
-  return new Ctx();
+module.exports = {
+  define: define,
+  resolve: resolve
 };
 
--- test.js
+++ test.js
@@ -2,26 +2,21 @@ var assert = require('assert');
 
 var ctx = require('./index');
 
-var $ = ctx();
-
-$.on('$binding', function (name, err, value) {
-  console.log(name, err, value);
-});
-
-$.define('a', function () { return 7; });
-$.define('b', function () { return 3; });
-$.define('c', function (a, b) { return a * b; });
-$.define('d', function () { return 2; });
-$.define('e', function (c, d, done) { return done(null, c * d); });
-$.define('test', function (a, b, c, d, e) {
+ctx.define('a', function () { return 7; });
+ctx.define('b', function () { return 3; });
+ctx.define('c', function (a, b) { return a * b; });
+ctx.define('d', function () { return 2; });
+ctx.define('e', function (c, d, done) { return done(null, c * d); });
+ctx.define('test', function (a, b, c, d, e) {
   assert.equal(a, 7);
   assert.equal(b, 3);
   assert.equal(c, 21);
   assert.equal(d, 2);
   assert.equal(e, 42);
+  console.log('OK');
 });
 
-$.resolve(function (err) {
+ctx.resolve(function (err) {
   console.log('errors', err);
 });
 
